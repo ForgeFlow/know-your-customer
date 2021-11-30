b0696cb08c48d68e20dbeb35c03fcde0c08d7078
Lois Rilo <lois.rilo@forgeflow.com>
2021-11-30 15:18:06 +0100

    [ADD] kyc: initial structure (alpha)

---
+++ kyc/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Know Your Customer",
+    "version": "14.0.1.0.0",
+    "summary": "Know Your Customer (KYC).",
+    "author": "ForgeFlow",
+    "website": "https://github.com/OCA/https://github.com/ForgeFlow/know-your-customer",
+    "license": "AGPL-3",
+    "development_status": "Alpha",
+    "category": "KYC",
+    "depends": ["webservice"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+}
