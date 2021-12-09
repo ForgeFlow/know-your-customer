32df6df95e7ea035fba470b063b658ef1f778591
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2021-12-09 15:55:53 +0530

    [ADD] kyc_account: Add kyc_account module

---
+++ kyc_account/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "KYC Account",
+    "version": "14.0.1.0.0",
+    "summary": "KYC Sale",
+    "author": "ForgeFlow",
+    "website": "https://github.com/OCA/https://github.com/ForgeFlow/know-your-customer",
+    "license": "AGPL-3",
+    "development_status": "Alpha",
+    "category": "KYC",
+    "depends": ["kyc", "account"],
+    "data": [],
+    "installable": True,
+}
