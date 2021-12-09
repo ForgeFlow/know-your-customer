afd4b6f29a77b7f909013c4060bc99517501ba0e
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2021-12-09 15:55:12 +0530

    [ADD] kyc_sale: Add kyc_sale module

---
+++ kyc_sale/models/sale.py
@@ -0,0 +1,15 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.onchange("partner_id")
+    def onchange_partner_id(self):
+        if self.partner_id and self.partner_id.is_expire:
+            raise UserError(_("%s's KYC status is expired." % (self.partner_id.name)))
+        return super(SaleOrder, self).onchange_partner_id()
