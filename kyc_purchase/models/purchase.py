92ed6a8ecad5304cdadc26a1fe20d7d638828a0d
Lois Rilo <lois.rilo@forgeflow.com>
2021-12-13 16:30:29 +0100

    [IMP] kyc_purchase: use centralized `_kyc_accept_transaction` method and check before confirming PO.

--- kyc_purchase/models/purchase.py
+++ kyc_purchase/models/purchase.py
@@ -1,8 +1,7 @@
 # Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, models
-from odoo.exceptions import UserError
+from odoo import api, models
 
 
 class PurchaseOrder(models.Model):
@@ -10,6 +9,10 @@ class PurchaseOrder(models.Model):
 
     @api.onchange("partner_id")
     def onchange_partner_id(self):
-        if self.partner_id and self.partner_id.is_expire:
-            raise UserError(_("%s's KYC status is expired." % (self.partner_id.name)))
-        return super(PurchaseOrder, self).onchange_partner_id()
+        if self.partner_id:
+            self.partner_id._kyc_accept_transaction()
+        return super().onchange_partner_id()
+
+    def button_confirm(self):
+        self.partner_id._kyc_accept_transaction()
+        return super().button_confirm()
