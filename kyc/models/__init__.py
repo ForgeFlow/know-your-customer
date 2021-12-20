67b3fc797915f375b9e82119eaa615145b5ee2e2
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2021-12-20 10:26:14 +0530

    [IMP] kyc: Update changes for document upload and check

--- kyc/models/__init__.py
+++ kyc/models/__init__.py
@@ -2,6 +2,6 @@ from . import res_partner
 from . import webservice_backend
 from . import res_company
 from . import res_config_settings
-from . import ir_attachment
 from . import kyc_log
 from . import kyc_ubo
+from . import kyc_document
