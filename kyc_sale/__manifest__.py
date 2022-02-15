16e952817c394e5a3a8c15c1ecc9eb1e1e85a06b
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-02-15 18:06:45 +0530

    [MIG] kyc_sale: Migration to 15.0

--- kyc_sale/__manifest__.py
+++ kyc_sale/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "KYC Sale",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "summary": "KYC Sale",
     "author": "ForgeFlow",
     "website": "https://github.com/ForgeFlow/know-your-customer",
