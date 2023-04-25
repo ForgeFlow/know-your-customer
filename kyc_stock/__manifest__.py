57eb0ebbfc5a165ae18e9c18eca24b3bb307cf3f
Lois Rilo <lois.rilo@forgeflow.com>
2023-04-25 10:18:00 +0200

    [ADD] kyc_stock

---
+++ kyc_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "KYC Stock",
+    "version": "14.0.1.0.0",
+    "summary": "KYC Sale",
+    "author": "ForgeFlow",
+    "website": "https://github.com/ForgeFlow/know-your-customer",
+    "license": "AGPL-3",
+    "development_status": "Beta",
+    "category": "KYC",
+    "depends": ["kyc", "stock"],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "installable": True,
+}
