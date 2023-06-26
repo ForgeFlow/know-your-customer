b64e8a17e4b72233bf8a9e0ba0c112192308fa56
DavidJForgeFlow <david.jimenez@forgeflow.com>
2023-06-26 10:47:41 +0200

    [IMP] kyc_sanction_scanner: add ongoing monitoring features

---
+++ kyc_sanction_scanner/models/res_partner.py
@@ -0,0 +1,31 @@
+# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from datetime import datetime
+
+from odoo import models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    def _get_scan_id_from_response(self, response):
+        res = super()._get_scan_id_from_response(response)
+        backend = self._get_kyc_webservice_backend()
+        if backend.tech_name == "kyc_sanction_scanner":
+            if len(response) != 1 and type(response) is not dict:
+                response = response[0]
+            if response.get("Result", {}).get("ReferenceNumber", False):
+                res = response["Result"]["ReferenceNumber"]
+        return res
+
+    def _get_next_ongoing_monitoring_from_response(self, response):
+        res = super()._get_next_ongoing_monitoring_from_response(response)
+        backend = self._get_kyc_webservice_backend()
+        if backend.tech_name == "kyc_sanction_scanner":
+            if len(response) != 1 and type(response) is not dict:
+                response = response[0]
+            if response.get("Result", {}).get("NextMonitoringDate", False):
+                date = (response["Result"]["NextMonitoringDate"]).split(".")[0]
+                res = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S")
+        return res
