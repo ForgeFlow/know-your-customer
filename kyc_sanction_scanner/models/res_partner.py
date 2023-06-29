e23b078269305a1c28d9de3571d951d9cae4c3a5
DavidJForgeFlow <david.jimenez@forgeflow.com>
2023-06-29 10:58:21 +0200

    [IMP] kyc_sanction_scanner: update scan_ids from old records

--- kyc_sanction_scanner/models/res_partner.py
+++ kyc_sanction_scanner/models/res_partner.py
@@ -1,6 +1,7 @@
 # Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+import re
 from datetime import datetime
 
 from odoo import models
@@ -17,6 +18,13 @@ class Partner(models.Model):
                 response = response[0]
             if response.get("Result", {}).get("ReferenceNumber", False):
                 res = response["Result"]["ReferenceNumber"]
+        return res
+
+    def _get_scan_id_from_text(self, text):
+        res = super()._get_scan_id_from_response(text)
+        backend = self._get_kyc_webservice_backend()
+        if backend.tech_name == "kyc_sanction_scanner":
+            res = re.findall("'ReferenceNumber': '([^']+)'", text)
         return res
 
     def _get_next_ongoing_monitoring_from_response(self, response):
