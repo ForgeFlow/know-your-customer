926de08073155a8b871b933350e38fdd547083ed
Lois Rilo <lois.rilo@forgeflow.com>
2023-07-05 17:27:53 +0200

    [IMP] kyc_account: only check top level parent, do not check specific invoice addresses

--- kyc_account/models/account.py
+++ kyc_account/models/account.py
@@ -10,13 +10,13 @@ class AccountMove(models.Model):
     @api.onchange("partner_id")
     def _onchange_partner_id(self):
         if self.partner_id:
-            self.partner_id._kyc_accept_transaction(self)
+            self.partner_id.commercial_partner_id._kyc_accept_transaction(self)
         return super()._onchange_partner_id()
 
     def _post(self, soft=True):
         for rec in self:
             if rec.partner_id:
-                rec.partner_id._kyc_accept_transaction(rec)
+                rec.partner_id.commercial_partner_id._kyc_accept_transaction(rec)
         return super()._post(soft=soft)
 
 
@@ -26,4 +26,4 @@ class AccountPayment(models.Model):
     @api.onchange("partner_id")
     def _onchange_partner_id(self):
         if self.partner_id:
-            self.partner_id._kyc_accept_transaction(self)
+            self.partner_id.commercial_partner_id._kyc_accept_transaction(self)
